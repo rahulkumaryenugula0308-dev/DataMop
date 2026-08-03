fcfa89b8f827b408e3ab769ee6483c705fb1ee0c
Your Name <you@example.com>
2026-08-03 14:04:08 +0530

    Initial project structure

---
+++ datamop/cleaner.py
@@ -0,0 +1,19 @@
+class DataCleaner:
+
+    def __init__(self):
+        pass
+
+    def load(self, filepath):
+        pass
+
+    def summarize(self):
+        pass
+
+    def clean(self):
+        pass
+
+    def save(self, filepath):
+        pass
+
+    def cleaning_log(self):
+        pass
