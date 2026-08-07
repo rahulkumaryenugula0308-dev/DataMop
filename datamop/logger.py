2d4ddcfc3cb02a135616f8b80bc756eb49e6fbd4
Your Name <you@example.com>
2026-08-07 14:26:35 +0530

    Added outlier, logger, and README skeleton

---
+++ datamop/logger.py
@@ -0,0 +1,6 @@
+def add_log(): 
+    pass 
+def show_log(): 
+    pass 
+def export_log(): 
+    pass 
---
+++ datamop/outliers.py
@@ -0,0 +1,10 @@
+def detect_iqr(): 
+    pass 
+def detect_zscore(): 
+    pass 
+def flag_outliers(): 
+    pass 
+def remove_outliers(): 
+    pass 
+def cap_outliers(): 
+    pass 
